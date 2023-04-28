8a1124eeeb8ef77ec803e78bab215d9ebe711fd9
rileymccarthy <riley.mccarthy@yahoo.com>
2023-04-28 03:16:44 +0000

    v5.9.19

--- include/libsys/dofmt.c
+++ include/libsys/dofmt.c
@@ -1,6 +1,16 @@
 #include <stdarg.h>
 #include <compiler.h>
+#include <sys/types.h>
 #include "sys/fmt.h"
+
+#ifdef __FLEXC__
+# ifdef __FEATURE_FLOATS__
+#  define INCLUDE_FLOATS
+# endif
+#else
+#define INCLUDE_FLOATS
+#endif
+#define LONGLONG_SUPPORT
 
 #define va_ptr          va_list *
 #define va_ptrarg(x, t) va_arg(*x, t)
@@ -114,12 +124,14 @@ union f_or_i {
     unsigned int i;
 };
 
+#ifdef INCLUDE_FLOATS
 static inline float _asfloat(unsigned int x)
 {
     union f_or_i v;
     v.i = x;
     return v.f;
 }
+#endif
 
 int _dofmt(putfunc fn, const char *fmtstr, va_list *args)
 {
@@ -135,12 +147,14 @@ int _dofmt(putfunc fn, const char *fmtstr, va_list *args)
     unsigned val;
 #ifdef LONGLONG_SUPPORT    
     unsigned long long val_LL;
+    int is_ll;
 #endif    
     for(;;) {
+        is_ll = 0;
         c = *fmtstr++;
         if (!c) break;
         if (c != '%') {
-            q = (*fn)(c);
+            q = PUTC(fn, c);
             if (q < 0) return q;
             bytes_written++;
             continue;
@@ -153,6 +167,8 @@ int _dofmt(putfunc fn, const char *fmtstr, va_list *args)
             fmtstr++;
             prec = parseint(&fmtstr, args) + 1;
             c = *fmtstr; if (c == 0) break;
+        } else {
+            prec = 0;
         }
         fmtstr = parsesize(fmtstr, &size);
         c = *fmtstr++; if (c == 0) break;
@@ -165,6 +181,8 @@ int _dofmt(putfunc fn, const char *fmtstr, va_list *args)
 #ifdef LONGLONG_SUPPORT        
         if (size == 8) {
             val_LL = va_ptrarg(args, unsigned long long);
+            val = val_LL;
+            is_ll = 1;
         }
         else
 #endif            
@@ -205,28 +223,42 @@ int _dofmt(putfunc fn, const char *fmtstr, va_list *args)
             if (prec == 0 && padchar == PADCHAR_ZERO) {
                 flags |= ((width+1)<<PREC_BIT);
             }
-            q = _fmtnum(fn, flags, val, 10);
+            if (!is_ll) {
+                q = _fmtnum(fn, flags, val, 10);
+            } else {
+                q = _fmtnumlong(fn, flags, val_LL, 10);
+            }
             break;
         case 'o':
             flags |= SIGNCHAR_UNSIGNED << SIGNCHAR_BIT;
             if (prec == 0 && padchar == PADCHAR_ZERO) {
                 flags |= ((width+1)<<PREC_BIT);
             }
-            q = _fmtnum(fn, flags, val, 8);
+            if (!is_ll) {
+                q = _fmtnum(fn, flags, val, 8);
+            } else {
+                q = _fmtnumlong(fn, flags, val_LL, 8);
+            }
             break;
         case 'x':
             if (prec == 0 && padchar == PADCHAR_ZERO) {
                 flags |= ((width+1)<<PREC_BIT);
             }
             flags |= SIGNCHAR_UNSIGNED << SIGNCHAR_BIT;
-            q = _fmtnum(fn, flags, val, 16);
+            if (!is_ll) {
+                q = _fmtnum(fn, flags, val, 16);
+            } else {
+                q = _fmtnumlong(fn, flags, val_LL, 16);
+            }
             break;
+#ifdef INCLUDE_FLOATS            
         case 'a':
         case 'e':
         case 'f':
         case 'g':
             q = _fmtfloat(fn, flags, _asfloat(val), c);
             break;
+#endif            
         default:
             q = _fmtstr(fn, flags, "???");
             break;
